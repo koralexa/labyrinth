dd6a3efe9a4966782bfd0b33daece2ec88864ee8
koralexa <koralexa@ispras.ru>
2021-02-24 21:50:51 +0300

    Deleted build files

--- DerivedData/Stray Cat/Build/Intermediates.noindex/Stray Cat.build/Debug/Stray Cat.build/Objects-normal/x86_64/main.d
+++
@@ -1,7 +0,0 @@
-dependencies: /Users/koralexa/Desktop/labyrinth/Stray\ Cat/main.cpp \
-  /Users/koralexa/Desktop/labyrinth/Stray\ Cat/common.h \
-  /Users/koralexa/Desktop/labyrinth/Stray\ Cat/glad/glad.h \
-  /Users/koralexa/Desktop/labyrinth/Stray\ Cat/glad/KHR/khrplatform.h \
-  /Users/koralexa/Desktop/labyrinth/Stray\ Cat/Image.h \
-  /Users/koralexa/Desktop/labyrinth/Stray\ Cat/Player.h \
-  /usr/local/include/GLFW/glfw3.h
